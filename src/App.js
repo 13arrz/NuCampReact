4141f57f22c980e473e0a0f944104b7bb203294f
13arrz <barry.mulingtapang@gmail.com>
2020-03-26 22:05:58 -0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
-import { CAMPSITES } from './shared/campsites';
-import './App.css';
-class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          campsites: CAMPSITES
-      };
-  }
+import React, { Component } from "react";
+import Main from "./components/MainComponent";
+import "./App.css";
 
+class App extends Component {
   render() {
     return (
-        <div className="App">
-            <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">NuCamp</NavbarBrand>
-                <Directory campsites={this.state.campsites} />
-            </div>
-            </Navbar>
-        </div>
+      <div className="App">
+        <Main />
+      </div>
     );
   }
 }
